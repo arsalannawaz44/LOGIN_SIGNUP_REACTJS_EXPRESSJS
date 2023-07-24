f8ed815da27306f5c096407ce0faf262bde6914f
arsalannawaz44 <arsalannawaz44@gmail.com>
2023-07-24 16:23:23 +0500

    Addede DropDown Logout Button

--- frontend/src/student_pages/studentNavebar.js
+++ frontend/src/student_pages/studentNavebar.js
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap'
+import StudentLogin from './studentLogin'
 
 export default function navbar(props) {
     const heading = props.heading;
@@ -31,6 +32,11 @@ export default function navbar(props) {
                             <LinkContainer to="/studentaddcourses">
                                 <Nav.Link>{Courses}</Nav.Link>
                             </LinkContainer>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="User Email">
+                            <NavDropdown.Item>Logout</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
